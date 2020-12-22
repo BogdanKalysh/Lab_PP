ccc1e017e4f55763685a35268cb93b8aa0bdfc0a
Bodya <bogdankalysh@Bodyas-MBP.Dlink>
2020-12-23 01:13:08 +0200

    lab_8 done

--- blueprints.py
+++ blueprints.py
@@ -1,11 +1,109 @@
 from flask import Blueprint, jsonify, request
 from schemas import *
 from models import *
-from flask_jwt_extended import create_access_token, jwt_required
-from werkzeug.security import check_password_hash
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_claims
+from werkzeug.security import check_password_hash, generate_password_hash
+import jwt
+
+blueprint = Blueprint("Rating",__name__)
 
 
-blueprint = Blueprint("Reiting",__name__)
+my_id = 0
+
+@blueprint.route("/teacher/signup",methods = ["POST"])
+def add_teacher():
+    try:
+        teacher_data = New_Teacher().load(request.json)
+        teach_obj = teacher(**teacher_data)
+        session = Session()
+        session.add(teach_obj)
+        session.commit()
+    except Exception:
+        return jsonify({"code":400,"error": "wrong data"})
+    
+    return jsonify(Teacher_Info().dump(teach_obj)),200
+
+
+@blueprint.route("/teacher/login", methods = ["GET"])
+def login_teacher():
+    try:
+        teacher_data = Teacher_Log_Info().load(request.json)
+
+        session = Session()
+        current_teacher = session.query(teacher).filter_by(email = teacher_data["email"]).first()
+
+        global my_id
+        my_id = current_teacher.teacher_id
+
+        print(my_id)
+
+        if check_password_hash(current_teacher.password, teacher_data["password"]):
+            return jsonify(access_token=create_access_token(identity = teacher_data["email"])), 200
+
+    except Exception:
+        return (jsonify({"code": 400 ,"error": "Wrong login info"}))
+    return (jsonify({"code": 400 ,"error": "Wrong login info"}))
+
+
+@blueprint.route("/teacher/logout", methods = ["GET"])
+@jwt_required
+def logout_teacher():
+    print(my_id)
+    return("Successful logout"), 200
+
+
+@blueprint.route("/teacher/<int:teacher_id>", methods = ["PUT"])
+@jwt_required
+def put_teacher(teacher_id):
+    try:
+        session = Session()
+
+        print(my_id)
+
+        teacher_data = New_Teacher().load(request.json)
+        original_teacher_data = session.query(teacher).filter_by(teacher_id = teacher_id).one()
+
+        teacher_data["password"] = generate_password_hash(teacher_data["password"])
+
+        if original_teacher_data == None:
+            return(jsonify({"code": 400 ,"error": "Wrong teacher id"}))
+
+        if(my_id != teacher_id):
+            return("No access to user")
+
+        for key , value in teacher_data.items():
+            setattr(original_teacher_data,key,value)
+
+        session.commit()
+
+    except Exception:
+        return(jsonify({"code":400,"error": "wrong teacher data"}))
+
+    return jsonify(New_Teacher().dump(original_teacher_data)),200
+
+
+@blueprint.route("/teacher/<int:teacher_id>", methods = ["DELETE"])
+@jwt_required
+def del_teacher(teacher_id):
+    try:
+        session = Session()
+
+        print(my_id)
+
+        if session.query(teacher).filter_by(teacher_id = teacher_id).one() == None:
+            return(jsonify({"code": 400 ,"error": "Wrong teacher id"}))
+
+        if(my_id != teacher_id):
+            return("No access to user")
+
+        session.query(teacher).filter_by(teacher_id = teacher_id).delete()
+        session.commit()
+    except Exception:
+        return(jsonify({"code": 400 ,"error": "Wrong teacher id"}))
+
+    return jsonify({"Teacher deleted ": 200})
+
+
 
 
 @blueprint.route("/students", methods = ["POST"])
@@ -92,73 +190,3 @@ def del_student(student_id):
     except Exception:
         return(jsonify({"code": 400 ,"error": "Wrong student id"}))
     return jsonify({"Student deleted ": 200})
-
-
-@blueprint.route("/teacher/signup",methods = ["POST"])
-def add_teacher():
-    try:
-        teacher_data = New_Teacher().load(request.json)
-        teach_obj = teacher(**teacher_data)
-        session = Session()
-        session.add(teach_obj)
-        session.commit()
-    except Exception:
-        return jsonify({"code":400,"error": "wrong data"})
-    
-    return jsonify(Teacher_Info().dump(teach_obj)),200
-
-
-@blueprint.route("/teacher/login", methods = ["GET"])
-def login_teacher():
-    try:
-        teacher_data = Teacher_Log_Info().load(request.json)
-
-        session = Session()
-        current_teacher = session.query(teacher).filter_by(email = teacher_data["email"]).one()
-
-
-        if check_password_hash(current_teacher.password, teacher_data["password"]):
-            return jsonify(access_token=create_access_token(identity = teacher_data["email"])), 200
-
-    except Exception:
-        return(jsonify({"code": 400 ,"error": "Wrong login info"}))
-
-
-@blueprint.route("/teacher/logout", methods = ["GET"])
-@jwt_required
-def logout_teacher():
-    return("Successful logout"), 200
-
-
-@blueprint.route("/teacher/<teacher_id>", methods = ["PUT"])
-def put_teacher(teacher_id):
-    try:
-        session = Session()
-
-        teacher_data = New_Teacher().load(request.json)
-        original_teacher_data = session.query(teacher).filter_by(teacher_id = teacher_id).one()
-
-        for key , value in teacher_data.items():
-            setattr(original_teacher_data,key,value)
-        session.commit()
-    except Exception:
-        return(jsonify({"code":400,"error": "wrong teacher data"}))
-
-    return jsonify(Teacher_Info().dump(original_teacher_data)),200
-
-
-@blueprint.route("/teacher/<teacher_id>", methods = ["DELETE"])
-@jwt_required
-def del_teacher(teacher_id):
-    try:
-        session = Session()
-
-        if session.query(teacher).filter_by(teacher_id = teacher_id).one() == None:
-            return(jsonify({"code": 400 ,"error": "Wrong teacher id"}))
-
-        session.query(teacher).filter_by(teacher_id = teacher_id).delete()
-        session.commit()
-    except Exception:
-        return(jsonify({"code": 400 ,"error": "Wrong teacher id"}))
-
-    return jsonify({"Teacher deleted ": 200})
--- schemas.py
+++ schemas.py
@@ -1,10 +1,10 @@
 from marshmallow import Schema, fields, post_load, validates
 
+
 class New_Student(Schema):
     name = fields.String(required = True)
     s_bal = fields.Integer()
     marks = fields.List(fields.Integer())
-
 
 class Student_Info(Schema):
     student_id = fields.Integer()
@@ -13,9 +13,9 @@ class Student_Info(Schema):
     marks = fields.List(fields.Integer())
 
 class New_Teacher(Schema):
-    name = fields.String(required = True)
-    email = fields.String()
-    password = fields.String()
+    name = fields.String()
+    email = fields.String(required = True)
+    password = fields.String(required = True)
 
 class Teacher_Log_Info(Schema):
     email = fields.String(required = True)
